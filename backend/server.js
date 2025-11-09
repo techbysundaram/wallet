d89c69c061c0edcad75cb8a8e4ebad4e7c029107
techbysundaram <techbysundaram@gmail.com>
2025-11-09 12:16:59 +0530

    setup neon postgres db with express backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10, 2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialised successfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1); //status code 1 means failure, 0 success
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Its working");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is running at ${PORT}`);
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App is running at ${PORT}`);
+  });
 });
